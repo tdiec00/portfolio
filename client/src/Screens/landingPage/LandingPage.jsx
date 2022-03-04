ea4b530db749afcb8bb8c970dff30873c34ba399
Trung Diec <tdiec00@gmail.com>
2022-03-03 19:30:27 -0500

    projects containers

--- client/src/Screens/landingPage/LandingPage.jsx
+++ client/src/Screens/landingPage/LandingPage.jsx
@@ -5,16 +5,16 @@ import ProjectPage from "../projectPage/ProjectPage"
 export default function LandingPage() {
   return (
     <div id="home" className="text-center bg-iTerm ">
-      <div className=" relative w-screen m-auto mt-1 h-screen text-left">
+      <div className=" relative w-screen m-auto mt-1 h-screen text-left ml-2">
         <h1 className="font-medium text-white font-mono  ... text-4xl   opacity-100 pt-3">~Welcome to iTrung</h1>
-        <h1 className="text-white text-2xl ">~cd portolio</h1>
-        <h1 className="text-white text-2xl">~npx create-react-app trung-portfolio</h1>
+        <h1 className="text-white text-2xl mt-5">~cd portolio</h1>
+        <h1 className="text-white text-2xl mt-2">~npx create-react-app trung-portfolio</h1>
         <div className="flex ... content-center items-center">
-          <h1 className="text-white text-2xl ">~Trung's Portfolio</h1>
+          <h1 className="text-white text-2xl mt-2">~Trung's Portfolio</h1>
           <div className="bg-white w-1.5 h-6 ml-1.5 animate__animated animate__flash  animate__infinite	infinite"></div>
         </div>
+        <ProjectPage />
       </div>
-      <ProjectPage />
     </div>
   )
 }
--- client/src/Screens/projectPage/ProjectPage.jsx
+++ client/src/Screens/projectPage/ProjectPage.jsx
@@ -1,5 +1,27 @@
 import React from "react"
 
 export default function ProjectPage() {
-  return <div id="project">ProjectPage</div>
+  return (
+    <div id="project" className="mt-14 text-white text-2xl text-center">
+      <h1>Projects</h1>
+      <div>
+        <div>
+          <a href="">Whats the Price</a>
+          <p>Description</p>
+        </div>
+        <div>
+          <a href="">Whats the Price</a>
+          <p>Description</p>
+        </div>
+        <div>
+          <a href="">Whats the Price</a>
+          <p>Description</p>
+        </div>
+        <div>
+          <a href="">Whats the Price</a>
+          <p>Description</p>
+        </div>
+      </div>
+    </div>
+  )
 }
