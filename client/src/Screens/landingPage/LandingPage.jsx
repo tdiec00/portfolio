ba093539a908bbd4cb0c79d7be7db64f61417680
Trung Diec <tdiec00@gmail.com>
2022-03-03 19:51:16 -0500

    project section design

--- client/src/Screens/landingPage/LandingPage.jsx
+++ client/src/Screens/landingPage/LandingPage.jsx
@@ -4,17 +4,17 @@ import ProjectPage from "../projectPage/ProjectPage"
 
 export default function LandingPage() {
   return (
-    <div id="home" className="text-center bg-iTerm ">
-      <div className=" relative w-screen m-auto mt-1 h-screen text-left ml-2">
-        <h1 className="font-medium text-white font-mono  ... text-4xl   opacity-100 pt-3">~Welcome to iTrung</h1>
-        <h1 className="text-white text-2xl mt-5">~cd portolio</h1>
-        <h1 className="text-white text-2xl mt-2">~npx create-react-app trung-portfolio</h1>
+    <div id="home" className="text-center bg-iTerm w-screen ">
+      <div className=" relative w-screen m-auto mt-1 h-96 text-left ">
+        <h1 className="font-medium text-white font-mono  ... text-4xl   opacity-100 pt-3 ml-2 pt-10">~Welcome to iTrung</h1>
+        <h1 className="text-white text-2xl mt-5 ml-2">~cd portolio</h1>
+        <h1 className="text-white text-2xl mt-2 ml-2">~npx create-react-app trung-portfolio</h1>
         <div className="flex ... content-center items-center">
-          <h1 className="text-white text-2xl mt-2">~Trung's Portfolio</h1>
+          <h1 className="text-white text-2xl mt-2 ml-2">~Trung's Portfolio</h1>
           <div className="bg-white w-1.5 h-6 ml-1.5 animate__animated animate__flash  animate__infinite	infinite"></div>
         </div>
-        <ProjectPage />
       </div>
+      <ProjectPage />
     </div>
   )
 }
--- client/src/Screens/projectPage/ProjectPage.jsx
+++ client/src/Screens/projectPage/ProjectPage.jsx
@@ -2,22 +2,22 @@ import React from "react"
 
 export default function ProjectPage() {
   return (
-    <div id="project" className="mt-14 text-white text-2xl text-center">
-      <h1>Projects</h1>
-      <div>
-        <div>
+    <div id="project" className="mt-14 text-white text-2xl text-center bg-blackbg w-screen">
+      <h1 className="pt-10 mb-10">Projects</h1>
+      <div className="flex flex-row flex-wrap justify-center">
+        <div className="h-80 bg-blackbg w-80">
           <a href="">Whats the Price</a>
           <p>Description</p>
         </div>
-        <div>
+        <div className="h-80 bg-blackbg w-80">
           <a href="">Whats the Price</a>
           <p>Description</p>
         </div>
-        <div>
+        <div className="h-80 bg-blackbg w-80">
           <a href="">Whats the Price</a>
           <p>Description</p>
         </div>
-        <div>
+        <div className="h-80 bg-blackbg w-80">
           <a href="">Whats the Price</a>
           <p>Description</p>
         </div>
