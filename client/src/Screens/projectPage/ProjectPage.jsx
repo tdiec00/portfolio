1bb471bd6e99cab59adbccc6b7e8c59650335929
Trung Diec <tdiec00@gmail.com>
2022-03-04 00:51:34 -0500

    styling updates

--- client/src/Screens/projectPage/ProjectPage.jsx
+++ client/src/Screens/projectPage/ProjectPage.jsx
@@ -5,21 +5,49 @@ export default function ProjectPage() {
     <div id="project" className=" text-white  text-center w-screen flex flex-col justify-center text-2xl">
       <h1 className="text-4xl mb-10">Projects</h1>
       <div className="flex flex-row flex-wrap justify-evenly">
-        <div className="h-80 bg-blackbg w-80 m-5 pt-5">
-          <a href="https://trungs-supermarket.netlify.app/">Whats the Price</a>
-          <p>Description</p>
+        <div className="h-96 bg-blackbg w-80 m-5 pt-3 ">
+          <a href="https://trungs-supermarket.netlify.app/">
+            <div>
+              <p className="pb-2">Whats the Price</p>
+              <p className="text-sm ">
+                A full stack cash register/shopping cart app where owners/employees are able to to add and manage grocery products. Customers are able to add, delete, and check out products from their
+                shopping cart.
+              </p>
+            </div>
+          </a>
         </div>
-        <div className="h-80 bg-blackbg w-80 m-5 pt-5">
-          <a href="">Whats the Price</a>
-          <p>Description</p>
+        <div className="h-96 bg-blackbg w-80 m-5 pt-3 ">
+          <a href="https://trungs-supermarket.netlify.app/">
+            <div>
+              <p className="pb-2">Whats the Price</p>
+              <p className="text-sm ">
+                A full stack cash register/shopping cart app where owners/employees are able to to add and manage grocery products. Customers are able to add, delete, and check out products from their
+                shopping cart.
+              </p>
+            </div>
+          </a>
         </div>
-        <div className="h-80 bg-blackbg w-80 m-5 pt-5">
-          <a href="">Whats the Price</a>
-          <p>Description</p>
+        <div className="h-96 bg-blackbg w-80 m-5 pt-3 ">
+          <a href="https://trungs-supermarket.netlify.app/">
+            <div>
+              <p className="pb-2">Whats the Price</p>
+              <p className="text-sm ">
+                A full stack cash register/shopping cart app where owners/employees are able to to add and manage grocery products. Customers are able to add, delete, and check out products from their
+                shopping cart.
+              </p>
+            </div>
+          </a>
         </div>
-        <div className="h-80 bg-blackbg w-80 m-5 pt-5">
-          <a href="">Whats the Price</a>
-          <p>Description</p>
+        <div className="h-96 bg-blackbg w-80 m-5 pt-3 ">
+          <a href="https://trungs-supermarket.netlify.app/">
+            <div>
+              <p className="pb-2">Whats the Price</p>
+              <p className="text-sm ">
+                A full stack cash register/shopping cart app where owners/employees are able to to add and manage grocery products. Customers are able to add, delete, and check out products from their
+                shopping cart.
+              </p>
+            </div>
+          </a>
         </div>
       </div>
     </div>
--- client/src/Screens/skillsPage/SkillsPage.jsx
+++ client/src/Screens/skillsPage/SkillsPage.jsx
@@ -1,11 +1,9 @@
-import React from "react"
-
 export default function SkillsPage() {
   return (
     <div id="skills" className=" text-white text-xl text-center flex flex-col justify-center h-60">
       <h1 className="text-4xl ">Skills</h1>
       <div className=" flex flex-wrap break-normal">
-        <p className="break-normal mx-44">
+        <p className="break-normal mx-80">
           JavaScript // React // Ruby // Rails // MongoDB // Mongoose // Express // PostgreSQL // SQL // HTML // CSS // Tailwind // BoostStrap // StyledComponents // Github // Heroku // Netlify //
           Node
         </p>
