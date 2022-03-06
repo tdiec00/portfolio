1e82329af9e11f1fcf8cc7f43509e39daaf9ff4e
Trung Diec <tdiec00@gmail.com>
2022-03-06 14:09:49 -0500

    box shadow modal

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -4,10 +4,10 @@ export default function Modal(props) {
   return (
     <div className={`modal ${props.show ? "show" : ""}`} onClick={() => props.onClose()}>
       <div className="flex fixed inset-0 bg-blackbg/[0.7] items-center justify-center">
-        <div className="w-3/5 h-3/5 bg-stone-400/[1]" onClick={(e) => e.stopPropagation()}>
+        <div className="w-3/5 h-3/5 bg-blackbg/[1] shadow-[0_0_14px_6px] shadow-white" onClick={(e) => e.stopPropagation()}>
           <div className="h-4/5 p-2.5 mt-">
             <h1 className="text-4xl mb-10 text-black ">{props.title}</h1>
-            <div className="h-4/5 p-2.5 border-y border-white">
+            <div className="h-4/5 p-2.5 ">
               {props.children}
               <div className="modal-footer"></div>
               <button
--- client/src/components/projectContainer/ProjectContainer.jsx
+++ client/src/components/projectContainer/ProjectContainer.jsx
@@ -6,21 +6,20 @@ export default function ProjectContainer(props) {
 
   return (
     <div className="h-80 bg-blackbg w-80 m-5 pt-3 ">
-      <a href={props.link}>
-        <div>
-          <p className="pb-2" onClick={() => setShow(true)}>
-            {props.title}
-          </p>
-          <Modal
-            onClose={() => {
-              props.setShow(false)
-            }}
-            show={show}
-          >
-            <p>{props.children}</p>
-          </Modal>
-        </div>
-      </a>
+      <div>
+        <p className="pb-2 cursor-pointer" onClick={() => setShow(true)}>
+          {props.title}
+        </p>
+        <Modal
+          onClose={() => {
+            setShow(false)
+          }}
+          show={show}
+          link={props.link}
+        >
+          <div>{props.children}</div>
+        </Modal>
+      </div>
     </div>
   )
 }
