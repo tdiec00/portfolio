0794ab8001b59099b726fba51d65d464d5417157
Trung Diec <tdiec00@gmail.com>
2022-03-06 18:41:51 -0500

    project desctription updates

--- client/src/Screens/projectPage/ProjectPage.jsx
+++ client/src/Screens/projectPage/ProjectPage.jsx
@@ -1,11 +1,12 @@
 import ProjectContainer from "../../components/projectContainer/ProjectContainer"
-import AthleticPerformanceIndex from "../../components/projects/AthleticPerformanceIndex"
 import CouchWorking from "../../components/projects/CouchWorking"
+import Athletic
 import WhatsThePrice from "../../components/projects/WhatsThePrice"
 import BrewMaster from "../../components/projects/BrewMaster"
 import couch from "../../images/couch.png"
 import supermarket from "../../images/supermarket.jpeg"
 import exercises from "../../images/exercises.png"
+import keg from "../../images/keg.png"
 
 export default function ProjectPage() {
   return (
@@ -18,10 +19,8 @@ export default function ProjectPage() {
         <ProjectContainer title="Couch Working" link="https://couchworking.netlify.app/" image={couch} alt="black and white couch">
           <CouchWorking />
         </ProjectContainer>
-        <ProjectContainer title="Athlethic Performance Index" link="https://athletic-performance-index.netlify.app/" image={exercises} alt="black and white dumbbell">
-          <AthleticPerformanceIndex />
-        </ProjectContainer>
-        <ProjectContainer title="Brew Master" link="https://tdiec00.github.io/Brew-Master-Index/">
+        <ProjectContainer title="Athlethic Performance Index" link="https://athletic-performance-index.netlify.app/" image={exercises} alt="black and white dumbbell"></ProjectContainer>
+        <ProjectContainer title="Brew Master" link="https://tdiec00.github.io/Brew-Master-Index/" image={keg} alt="black and white beer keg">
           <BrewMaster />
         </ProjectContainer>
       </div>
