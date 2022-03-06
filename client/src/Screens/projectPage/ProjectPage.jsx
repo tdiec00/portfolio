188d8a303779a5af4619496b5963e20f0b802e88
Trung Diec <tdiec00@gmail.com>
2022-03-06 15:19:02 -0500

    uploaded third image

--- client/src/Screens/projectPage/ProjectPage.jsx
+++ client/src/Screens/projectPage/ProjectPage.jsx
@@ -4,19 +4,21 @@ import CouchWorking from "../../components/projects/CouchWorking"
 import WhatsThePrice from "../../components/projects/WhatsThePrice"
 import BrewMaster from "../../components/projects/BrewMaster"
 import couch from "../../images/couch.png"
+import supermarket from "../../images/supermarket.jpeg"
+import exercises from "../../images/exercises.png"
 
 export default function ProjectPage() {
   return (
     <div id="project" className=" text-white  text-center w-screen flex flex-col justify-center text-2xl">
       <h1 className="text-4xl mb-10">Projects</h1>
       <div className="flex flex-row flex-wrap justify-evenly">
-        <ProjectContainer title="What's the Price" link="https://trungs-supermarket.netlify.app/">
+        <ProjectContainer title="What's the Price" link="https://trungs-supermarket.netlify.app/" image={supermarket} alt="black and white supermarket">
           <WhatsThePrice />
         </ProjectContainer>
         <ProjectContainer title="Couch Working" link="https://couchworking.netlify.app/" image={couch} alt="black and white couch">
           <CouchWorking />
         </ProjectContainer>
-        <ProjectContainer title="Athlethic Performance Index" link="https://athletic-performance-index.netlify.app/">
+        <ProjectContainer title="Athlethic Performance Index" link="https://athletic-performance-index.netlify.app/" image={exercises} alt="black and white dumbbell">
           <AthleticPerformanceIndex />
         </ProjectContainer>
         <ProjectContainer title="Brew Master" link="https://tdiec00.github.io/Brew-Master-Index/">
--- client/src/components/projectContainer/ProjectContainer.jsx
+++ client/src/components/projectContainer/ProjectContainer.jsx
@@ -1,16 +1,17 @@
 import Modal from "../modal/Modal"
 import {useState} from "react"
+
 export default function ProjectContainer(props) {
   const [show, setShow] = useState(false)
 
   return (
-    <div className="h-60 bg-blackbg w-80 m-5 pt-3 ">
+    <div className="h-72 bg-blackbg w-80 m-5 pt-3 ">
       <div>
         <p className="pb-2 cursor-pointer" onClick={() => setShow(true)}>
           {props.title}
         </p>
         <div className="flex justify-center mt-6">
-          <img className="w-9/12" src={props.image} alt={props.alt}></img>
+          <img className="w-9/12 h-44" src={props.image} alt={props.alt}></img>
         </div>
         <Modal
           onClose={() => {
