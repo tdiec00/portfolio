cffdeba2c6b24dc9044b88a1f5074728e1630694
Trung Diec <tdiec00@gmail.com>
2022-03-06 19:09:51 -0500

    reveresed the slashes

--- client/src/Screens/skillsPage/SkillsPage.jsx
+++ client/src/Screens/skillsPage/SkillsPage.jsx
@@ -3,10 +3,9 @@ export default function SkillsPage() {
     <div id="skills" className=" text-white text-xl text-center flex flex-col justify-center mb-5 mt-5 ">
       <h1 className="text-4xl mb-5">Skills</h1>
       <div className="flex flex-wrap break-normal mx-80">
-        // eslint-disable-next-line
         <p>
-          JavaScript // React // Ruby // Rails // MongoDB // Mongoose // Express // PostgreSQL // SQL // HTML // CSS // Tailwind // BoostStrap // React Styled Components // Github // Heroku // Netlify
-          // Node
+          JavaScript \\ React \\ Ruby \\ Rails \\ MongoDB \\ Mongoose \\ Express \\ PostgreSQL \\ SQL \\ HTML \\ CSS \\ Tailwind \\ BoostStrap \\ React Styled Components \\ Github \\ Heroku \\ Netlify
+          \\ Node
         </p>
       </div>
     </div>
