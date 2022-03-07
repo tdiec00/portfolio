36ee123cb3d3f0940336d28fe82b17f03eb6e88c
Trung Diec <tdiec00@gmail.com>
2022-03-06 23:37:02 -0500

    description updates

--- client/src/components/projects/AthleticPerformanceIndex.jsx
+++ client/src/components/projects/AthleticPerformanceIndex.jsx
@@ -2,15 +2,15 @@ export default function AthleticPerformanceIndex() {
   return (
     <div className="text-base md:text-lg md:px-10">
       <p className="md:mt-10">
-        A gorcery store shopping application where customers are able to add items to and check out items from their cart. Admins have the ability to add and edit products in the grocery store
-        database.
+        This application contains a list of exercises broken down by body parts. Users can add to the the existing lists of exercises. There is also an upvote fuctionality and each page displays a
+        list of the top 5 exercises for each body part.
       </p>
       <br />
-      <a className="hover:text-rose-700" href="https://trungs-supermarket.netlify.app/">
+      <a className="hover:text-rose-700" href="https://athletic-performance-index.netlify.app">
         Deployed Website
       </a>
       <br />
-      <a className="hover:text-rose-700" href="https://github.com/tdiec00/Whats-the-Total">
+      <a className="hover:text-rose-700" href="https://github.com/tdiec00/Athletic-Performance-Index">
         Github Link
       </a>
     </div>
--- client/src/components/projects/BrewMaster.jsx
+++ client/src/components/projects/BrewMaster.jsx
@@ -2,15 +2,14 @@ export default function BrewMaster() {
   return (
     <div className="text-base md:text-lg md:px-10">
       <p className="md:mt-10">
-        A gorcery store shopping application where customers are able to add items to and check out items from their cart. Admins have the ability to add and edit products in the grocery store
-        database.
+        An online database consisting of random beer recipes for beginners and amateurs. Each beer includes a short description along with ingredients and specific temparatures for brewing.
       </p>
       <br />
-      <a className="hover:text-rose-700" href="https://trungs-supermarket.netlify.app/">
+      <a className="hover:text-rose-700" href="https://tdiec00.github.io/Brew-Master-Index/">
         Deployed Website
       </a>
       <br />
-      <a className="hover:text-rose-700" href="https://github.com/tdiec00/Whats-the-Total">
+      <a className="hover:text-rose-700" href="https://github.com/tdiec00/Brew-Master-Index">
         Github Link
       </a>
     </div>
